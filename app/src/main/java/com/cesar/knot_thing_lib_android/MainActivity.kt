14359f1af2ee6485298a6764d66d3dc079b830bc
Lucas Cabral <lcc2@cesar.org.br>
2020-02-06 15:33:29 +0000

    Add KNoT state machine
    Add state machine that handles incoming messages and notifies the
    states of events.
    
    This is the class that manages the states, it's acts as an interface
    between the state and the events.

--- app/src/main/java/com/cesar/knot_thing_lib_android/MainActivity.kt
+++ app/src/main/java/com/cesar/knot_thing_lib_android/MainActivity.kt
@@ -2,88 +2,13 @@ package com.cesar.knot_thing_lib_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.cesar.knot_sdk.KNoTAMQP
-import com.cesar.knot_sdk.KNoTAMQPFactory
-import com.cesar.knot_sdk.KNoTMessager
-import com.cesar.knot_sdk.knot_messages.KNoTMessageRegister
-import com.cesar.knot_sdk.knot_messages.KNoTMessageUnregister
-import com.cesar.knot_sdk.knot_messages.KNoTMessageAuth
-import com.cesar.knot_sdk.KNoTTypes.KNOT_TYPE_ID_SWITCH
-import com.cesar.knot_sdk.KNoTTypes.KNOT_UNIT_NOT_APPLICABLE
-import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_TYPE_BOOL
-import com.cesar.knot_sdk.knot_data.KNoTSchema
-import com.cesar.knot_sdk.knot_messages.KNoTMessageUpdateSchema
-import com.cesar.knot_sdk.knot_messages.KNoTMessageDataItem
-import com.cesar.knot_sdk.knot_messages.KNoTMessageUpdateData
-import kotlinx.android.synthetic.main.activity_main.register_button
-import kotlinx.android.synthetic.main.activity_main.unregister_button
-import kotlinx.android.synthetic.main.activity_main.authenticate_button
-import kotlinx.android.synthetic.main.activity_main.schema_update_button
-import kotlinx.android.synthetic.main.activity_main.data_publish_button
-import org.jetbrains.anko.doAsync
+import com.cesar.knot_sdk.knot_state_machine.KNoTStateMachine
 
 class MainActivity : AppCompatActivity() {
-    val HOSTNAME = "0.0.0.0"
-    val PORT_NUMBER = 5672
-    val USERNAME = "user"
-    val PASSWORD = "pass"
-    val THING_ID = "a74151d19de59cd3"
-    val THING_NAME = "thing-name"
-    val THING_TOKEN = "ejfhwekhrui234huirh23uf"
-    val SENSOR_ID = 1
-    val SENSOR_NAME = "updateSchemaTest"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val knotAMQP = KNoTAMQP(USERNAME, PASSWORD, HOSTNAME, PORT_NUMBER)
-        lateinit var knotMessager : KNoTMessager
-        KNoTAMQPFactory().create(knotAMQP)
-        val knotThingRegister = KNoTMessageRegister(THING_ID, THING_NAME)
-        val knotThingUnregister = KNoTMessageUnregister(THING_ID)
-        val knotThingAuth = KNoTMessageAuth(THING_ID, THING_TOKEN)
-        val knotThingSchema = mutableListOf(
-            KNoTSchema(
-                SENSOR_ID,
-                KNOT_VALUE_TYPE_BOOL,
-                KNOT_UNIT_NOT_APPLICABLE,
-                KNOT_TYPE_ID_SWITCH,
-                SENSOR_NAME
-            )
-        )
-
-        val knotThingUpdateSchema = KNoTMessageUpdateSchema(THING_ID, knotThingSchema)
-
-
-        val dataItem = object {
-            val sensorId : Int = SENSOR_ID
-            var value : Boolean = true
-        }
-
-        val knotData = mutableListOf(
-            KNoTMessageDataItem(dataItem.sensorId, dataItem.value)
-        )
-        val knotThingUpdateData = KNoTMessageUpdateData(THING_ID, knotData)
-
-        register_button.setOnClickListener {
-            doAsync { knotMessager.register(knotThingRegister) }
-        }
-
-        unregister_button.setOnClickListener {
-            doAsync { knotMessager.unregister(knotThingUnregister) }
-        }
-
-        authenticate_button.setOnClickListener {
-            doAsync { knotMessager.authenticate(knotThingAuth) }
-        }
-
-        schema_update_button.setOnClickListener {
-            doAsync { knotMessager.updateSchema(knotThingUpdateSchema) }
-        }
-
-        data_publish_button.setOnClickListener {
-            doAsync { knotMessager.publishData(knotThingUpdateData) }
-        }
-
+        val knotStateMachine = KNoTStateMachine
     }
 }
---
+++ knot-thing/src/main/java/com/cesar/knot_sdk/knot_state_machine/KNoTStateMachine.kt
@@ -0,0 +1,61 @@
+package com.cesar.knot_sdk.knot_state_machine
+
+import android.content.Context
+import com.cesar.knot_sdk.KNoTAMQP
+import com.cesar.knot_sdk.KNoTDataManager
+import com.cesar.knot_sdk.KNoTMessageParser
+import com.cesar.knot_sdk.KNoTMessager
+import com.cesar.knot_sdk.knot_state_machine.states.base_classes.State
+import com.cesar.knot_sdk.knot_state_machine.states.base_classes.State.Companion.PROCEED_MESSAGE
+import com.cesar.knot_sdk.knot_state_machine.states.base_classes.branching_state.BranchingState
+import com.cesar.knot_sdk.knot_state_machine.states.base_classes.procedural_state.ProceduralState
+import com.cesar.knot_sdk.knot_state_machine.states.base_classes.waiting_state.WaitingState
+
+object KNoTStateMachine {
+
+    const val DEFAULT_RABBIT_PORT_NUMBER = 5672
+    const val HOSTNAME = ""
+    const val PORT_NUMBER = DEFAULT_RABBIT_PORT_NUMBER
+    const val USERNAME = ""
+    const val PASSWORD = ""
+
+    val knotAMQP = KNoTAMQP(USERNAME, PASSWORD, HOSTNAME, PORT_NUMBER)
+    lateinit var state : State
+    lateinit var knotMessager : KNoTMessager
+    lateinit var knotDataManager : KNoTDataManager
+    lateinit var knotMessageParser : KNoTMessageParser
+    lateinit var context : Context
+    lateinit var thingName : String
+
+    val PREF_ID = "preference file key"
+    val UUID_PREF = "uuid"
+    val TOKEN_PREF = "token"
+    val SCHEMA_SENT = "schema sent"
+    val PREF_MISSING = ""
+
+    val sharedPref = context.getSharedPreferences(
+        PREF_ID,
+        Context.MODE_PRIVATE
+    )
+
+    fun operationResult(message : String = PROCEED_MESSAGE) {
+        state = state.getNextState(message)
+    }
+
+    private fun waitForResponse() {
+        TODO("This method will create a timer that checks for a timeout for the request")
+    }
+
+    fun enterState() {
+        state.enter()
+
+        when(state) {
+            is ProceduralState -> operationResult()
+            is WaitingState    -> waitForResponse()
+            is BranchingState  -> operationResult()
+        }
+    }
+
+    fun getUUID() = sharedPref.getString(UUID_PREF, PREF_MISSING)
+
+}
