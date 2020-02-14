55e95b49ae9c8d01b895cba3f27b60c4ae96c43c
Lucas Cabral <lcc2@cesar.org.br>
2020-02-14 18:22:24 +0000

    Add KNoT state machine
    Add state machine that handles incoming messages and notifies the
    states of events.
    
    This is the class that manages the states, it's acts as an interface
    between the state and the events.

---
+++ knot-thing/src/main/java/com/cesar/knot_sdk/knot_state_machine/KNoTStateMachine.kt
@@ -0,0 +1,53 @@
+package com.cesar.knot_sdk.knot_state_machine
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.cesar.knot_sdk.KNoTAMQP
+import com.cesar.knot_sdk.KNoTDataManager
+import com.cesar.knot_sdk.KNoTMessageParser
+import com.cesar.knot_sdk.KNoTMessager
+import com.cesar.knot_sdk.knot_state_machine.states.base_classes.State
+import com.cesar.knot_sdk.knot_state_machine.states.base_classes.State.Companion.PROCEED_MESSAGE
+
+object KNoTStateMachine {
+
+    var DEFAULT_RABBIT_PORT_NUMBER = 5672
+    var HOSTNAME = ""
+    var PORT_NUMBER = DEFAULT_RABBIT_PORT_NUMBER
+    var USERNAME = ""
+    var PASSWORD = ""
+
+    lateinit var state : State
+    lateinit var knotMessager : KNoTMessager
+    lateinit var knotAMQP : KNoTAMQP
+    lateinit var knotDataManager : KNoTDataManager
+    val knotMessageParser = KNoTMessageParser()
+    lateinit var context : Context
+    lateinit var thingName : String
+
+    fun schemaChanged() = true
+
+    val PREF_ID = "preference file key"
+    val UUID_PREF = "uuid"
+    val TOKEN_PREF = "token"
+    val PREF_MISSING = ""
+
+    lateinit var sharedPref : SharedPreferences
+
+    fun operationResult(message : String = PROCEED_MESSAGE) {
+    }
+
+    private fun waitForResponse() {
+        TODO("This method will create a timer that checks for a timeout for the request")
+    }
+
+    fun enterState() {
+        state.enter()
+    }
+
+    fun getUUID() = sharedPref.getString(
+        UUID_PREF,
+        PREF_MISSING
+    )
+
+}
