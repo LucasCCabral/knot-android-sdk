30817daa7042db60531e798ac41dc8deb9f9a36b
Lucas Cabral <lcc2@cesar.org.br>
2020-02-14 18:15:55 +0000

    Add base classes for the states of a machine
    Add abstract class that represent the different characteristics that a
    state can have and the class that defines what a event is.
    
    This was done to help the creation of the state and provide guaranteed
    behavior for the state machine.

---
+++ knot-thing/src/main/java/com/cesar/knot_sdk/knot_state_machine/states/base_classes/KNoTEvent.kt
@@ -0,0 +1,19 @@
+package com.cesar.knot_sdk.knot_state_machine.states.base_classes
+
+import com.cesar.knot_sdk.knot_messages.KNoTMessageAuthenticated
+import com.cesar.knot_sdk.knot_messages.KNoTMessageRegistered
+import com.cesar.knot_sdk.knot_messages.KNoTMessageSchemaResp
+import com.cesar.knot_sdk.knot_messages.KNoTMessageUnregister
+
+sealed class KNoTEvent {
+    object NotReady                                        : KNoTEvent()
+    object Ready                                           : KNoTEvent()
+    object Timeout                                         : KNoTEvent()
+    class RegOK(val res : KNoTMessageRegistered)           : KNoTEvent()
+    class RegNotOk(val res : KNoTMessageRegistered)        : KNoTEvent()
+    class AuthNotOk(val res : KNoTMessageAuthenticated)    : KNoTEvent()
+    class AuthOk(val res : KNoTMessageAuthenticated)       : KNoTEvent()
+    class SchemaOk(val res : KNoTMessageSchemaResp)        : KNoTEvent()
+    class SchemaNotOk(val res : KNoTMessageSchemaResp)     : KNoTEvent()
+    class UnregisterEvent(val res : KNoTMessageUnregister) : KNoTEvent()
+}
