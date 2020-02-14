7ea121b041c2c229e40f94c97adb7207edbc93e9
Lucas Cabral <lcc2@cesar.org.br>
2020-02-14 19:03:51 +0000

    Add tests for the register state
    Add tests for all possible events that the register state should be
    able to handle.
     Please enter the commit message for your changes. Lines starting

---
+++ knot-thing/src/test/java/com/cesar/knot_sdk/RegisterTest.kt
@@ -0,0 +1,144 @@
+package com.cesar.knot_sdk
+
+import com.cesar.knot_sdk.knot_messages.KNoTMessageAuthenticated
+import com.cesar.knot_sdk.knot_messages.KNoTMessageRegistered
+import com.cesar.knot_sdk.knot_messages.KNoTMessageSchemaResp
+import com.cesar.knot_sdk.knot_messages.KNoTMessageUnregister
+import com.cesar.knot_sdk.knot_state_machine.KNoTStateMachine
+import com.cesar.knot_sdk.knot_state_machine.states.Disconnected
+import com.cesar.knot_sdk.knot_state_machine.states.Registering
+import com.cesar.knot_sdk.knot_state_machine.states.Schema
+import com.cesar.knot_sdk.knot_state_machine.states.base_classes.KNoTEvent.*
+import io.mockk.every
+import io.mockk.mockkObject
+import org.junit.Test
+
+class RegisterTest {
+
+    private val randomThingId = "0123456789abcde"
+    private val nullError = "null"
+    private val error = "non null error"
+    private val randomToken = "random token"
+    private val state = Registering()
+
+    @Test
+    fun timeoutEvent_isRegistering() {
+        val timeoutEvent = Timeout
+        val nextState = state.getNextState(timeoutEvent)
+
+        assert(nextState is Registering)
+    }
+
+    @Test
+    fun notReadyEvent_isDisconnected() {
+        val notReadyEvent = NotReady
+        val nextState = state.getNextState(notReadyEvent)
+
+        assert(nextState is Disconnected)
+    }
+
+    @Test
+    fun readyEvent_withoutCredentials_isRegistering() {
+        val readyEvent = Ready
+        val nextState = state.getNextState(readyEvent)
+
+        assert(nextState is Registering)
+    }
+
+    @Test
+    fun readyEvent_withCredentials_isRegistering() {
+        val readyEvent = Ready
+        val nextState = state.getNextState(readyEvent)
+
+        assert(nextState is Registering)
+    }
+
+    @Test
+    fun authOkEvent_isRegistering() {
+        val authOkMessage = KNoTMessageAuthenticated(
+                randomThingId,
+                nullError
+        )
+        val authOkEvent = AuthOk(authOkMessage)
+        val nextState = state.getNextState(authOkEvent)
+
+        assert(nextState is Registering)
+    }
+
+    @Test
+    fun authNotOkEvent_isRegistering() {
+        val authNotOkMessage = KNoTMessageAuthenticated(
+                randomThingId,
+                error
+        )
+        val authNotOkEvent = AuthNotOk(authNotOkMessage)
+        val nextState = state.getNextState(authNotOkEvent)
+
+        assert(nextState is Registering)
+    }
+
+    @Test
+    fun regOkEvent_isSchema() {
+        val regOkMessage = KNoTMessageRegistered(
+                randomThingId,
+                randomToken,
+                nullError
+        )
+        val regOkEvent = RegOK(regOkMessage)
+
+        mockkObject(KNoTStateMachine)
+        every { KNoTStateMachine.storeCredentials(regOkMessage) } returns Unit
+
+        val nextState = state.getNextState(regOkEvent)
+        assert(nextState is Schema)
+    }
+
+    @Test
+    fun regNotOkEvent_isRegistering() {
+        val regNotOkMessage = KNoTMessageRegistered(
+                randomThingId,
+                randomToken,
+                error
+        )
+        val regNotOkEvent = RegNotOk(regNotOkMessage)
+        val nextState = state.getNextState(regNotOkEvent)
+
+        assert(nextState is Registering)
+    }
+
+    @Test
+    fun schemaOkEvent_isRegistering() {
+        val schemaOkMessage = KNoTMessageSchemaResp(
+                randomThingId,
+                nullError
+        )
+        val schemaOkEvent = SchemaOk(schemaOkMessage)
+        val nextState = state.getNextState(schemaOkEvent)
+
+        assert(nextState is Registering)
+    }
+
+    @Test
+    fun schemaNotOkEvent_isRegistering() {
+        val schemaOkMessage = KNoTMessageSchemaResp(
+                randomThingId,
+                error
+        )
+        val schemaOkEvent = SchemaNotOk(schemaOkMessage)
+        val nextState = state.getNextState(schemaOkEvent)
+
+        assert(nextState is Registering)
+    }
+
+    @Test
+    fun unregisterEvent_correctId_isRegistering() {
+        val unregisterMessage = KNoTMessageUnregister(
+                randomThingId
+        )
+        val unregisterEvent = UnregisterEvent(unregisterMessage)
+        val nextState = state.getNextState(unregisterEvent)
+
+        assert(nextState is Registering)
+    }
+
+}
