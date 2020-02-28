dcb53db46f466487ef9033babca881df850472ee
Lucas Cabral <lcc2@cesar.org.br>
2020-02-28 17:48:07 +0000

    Add toy application to display usage of the SDK

--- app/src/main/java/com/cesar/knot_thing_lib_android/MainActivity.kt
+++ app/src/main/java/com/cesar/knot_thing_lib_android/MainActivity.kt
@@ -2,88 +2,53 @@ package com.cesar.knot_thing_lib_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.cesar.knot_sdk.KNoTAMQP
-import com.cesar.knot_sdk.KNoTAMQPFactory
-import com.cesar.knot_sdk.KNoTMessager
-import com.cesar.knot_sdk.knot_messages.KNoTMessageRegister
-import com.cesar.knot_sdk.knot_messages.KNoTMessageUnregister
-import com.cesar.knot_sdk.knot_messages.KNoTMessageAuth
-import com.cesar.knot_sdk.KNoTTypes.KNOT_TYPE_ID_SWITCH
+import com.cesar.knot_sdk.KNoTThingBuilder
+import com.cesar.knot_sdk.KNoTTypes.KNOT_TYPE_ID_NONE
 import com.cesar.knot_sdk.KNoTTypes.KNOT_UNIT_NOT_APPLICABLE
-import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_TYPE_BOOL
-import com.cesar.knot_sdk.knot_data.KNoTSchema
-import com.cesar.knot_sdk.knot_messages.KNoTMessageUpdateSchema
-import com.cesar.knot_sdk.knot_messages.KNoTMessageDataItem
-import com.cesar.knot_sdk.knot_messages.KNoTMessageUpdateData
-import kotlinx.android.synthetic.main.activity_main.register_button
-import kotlinx.android.synthetic.main.activity_main.unregister_button
-import kotlinx.android.synthetic.main.activity_main.authenticate_button
-import kotlinx.android.synthetic.main.activity_main.schema_update_button
-import kotlinx.android.synthetic.main.activity_main.data_publish_button
-import org.jetbrains.anko.doAsync
+import com.cesar.knot_sdk.KNoTTypes.KNOT_VALUE_TYPE_INT
+import com.cesar.knot_sdk.knot_data.KNoTData
+import com.cesar.knot_sdk.knot_data.KNoTValueInt
+import com.cesar.knot_sdk.knot_data.builder.KNoTDataBuilder
 
 class MainActivity : AppCompatActivity() {
-    val HOSTNAME = "0.0.0.0"
+    val HOSTNAME = "192.168.31.123"
     val PORT_NUMBER = 5672
-    val USERNAME = "user"
-    val PASSWORD = "pass"
-    val THING_ID = "a74151d19de59cd3"
+    val USERNAME = "lucas"
+    val PASSWORD = "lucas"
     val THING_NAME = "thing-name"
-    val THING_TOKEN = "ejfhwekhrui234huirh23uf"
     val SENSOR_ID = 1
     val SENSOR_NAME = "updateSchemaTest"
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val knotAMQP = KNoTAMQP(USERNAME, PASSWORD, HOSTNAME, PORT_NUMBER)
-        lateinit var knotMessager : KNoTMessager
-        KNoTAMQPFactory().create(knotAMQP)
-        val knotThingRegister = KNoTMessageRegister(THING_ID, THING_NAME)
-        val knotThingUnregister = KNoTMessageUnregister(THING_ID)
-        val knotThingAuth = KNoTMessageAuth(THING_ID, THING_TOKEN)
-        val knotThingSchema = mutableListOf(
-            KNoTSchema(
-                SENSOR_ID,
-                KNOT_VALUE_TYPE_BOOL,
-                KNOT_UNIT_NOT_APPLICABLE,
-                KNOT_TYPE_ID_SWITCH,
-                SENSOR_NAME
-            )
-        )
 
-        val knotThingUpdateSchema = KNoTMessageUpdateSchema(THING_ID, knotThingSchema)
+        val lowerBound = KNoTValueInt(SENSOR_ID, 0)
+        val upperBound = KNoTValueInt(SENSOR_ID, 10)
+        val currValue = KNoTValueInt(SENSOR_ID, 5)
 
+        val knotData = KNoTDataBuilder<KNoTValueInt>()
+            .setName(SENSOR_NAME)
+            .setSensorId(SENSOR_ID)
+            .setValueType(KNOT_VALUE_TYPE_INT)
+            .setUnit(KNOT_UNIT_NOT_APPLICABLE)
+            .setTypeId(KNOT_TYPE_ID_NONE)
+            .setLowerBound(lowerBound)
+            .setUpperBound(upperBound)
+            .setValue(currValue)
+            .build()
 
-        val dataItem = object {
-            val sensorId : Int = SENSOR_ID
-            var value : Boolean = true
-        }
+        val knotThing = KNoTThingBuilder()
+            .setHostname(HOSTNAME)
+            .setUsername(USERNAME)
+            .setPassword(PASSWORD)
+            .setPortNumber(PORT_NUMBER)
+            .setContext(applicationContext)
+            .setThingName(THING_NAME)
+            .addSensor(knotData as KNoTData<Any>)
+            .build()
 
-        val knotData = mutableListOf(
-            KNoTMessageDataItem(dataItem.sensorId, dataItem.value)
-        )
-        val knotThingUpdateData = KNoTMessageUpdateData(THING_ID, knotData)
-
-        register_button.setOnClickListener {
-            doAsync { knotMessager.register(knotThingRegister) }
-        }
-
-        unregister_button.setOnClickListener {
-            doAsync { knotMessager.unregister(knotThingUnregister) }
-        }
-
-        authenticate_button.setOnClickListener {
-            doAsync { knotMessager.authenticate(knotThingAuth) }
-        }
-
-        schema_update_button.setOnClickListener {
-            doAsync { knotMessager.updateSchema(knotThingUpdateSchema) }
-        }
-
-        data_publish_button.setOnClickListener {
-            doAsync { knotMessager.publishData(knotThingUpdateData) }
-        }
+        knotThing.enterState()
 
     }
 }
